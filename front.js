6e70803d49557814bcd3245ac9fe3f5fdc02957d
uncreative <ayelet.goldin@gmail.com>
2018-08-13 01:51:06 -0700

    Initial commit

---
+++ front.js
@@ -0,0 +1,221 @@
+String.prototype.hashCode = function() {
+  var hash = 0,
+    i,
+    chr,
+    len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr = this.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+getSeed = function(s) {
+  s += Math.floor(new Date().getTime() / 120000);
+  return s;
+};
+
+// https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+Alea = function(seed) {
+  if (seed === undefined) {
+    seed = +new Date() + Math.random();
+  }
+
+  function Mash() {
+    var n = 4022871197;
+    return function(r) {
+      for (var t, s, u = 0, e = 0.02519603282416938; u < r.length; u++)
+        (s = r.charCodeAt(u)),
+          (f = e * (n += s) - ((n * e) | 0)),
+          (n = 4294967296 * ((t = f * ((e * n) | 0)) - (t | 0)) + (t | 0));
+      return (n | 0) * 2.3283064365386963e-10;
+    };
+  }
+  return (function() {
+    var m = Mash(),
+      a = m(" "),
+      b = m(" "),
+      c = m(" "),
+      x = 1,
+      y;
+    (seed = seed.toString()), (a -= m(seed)), (b -= m(seed)), (c -= m(seed));
+    a < 0 && a++, b < 0 && b++, c < 0 && c++;
+    return function() {
+      var y = x * 2.3283064365386963e-10 + a * 2091639;
+      (a = b), (b = c);
+      return (c = y - (x = y | 0));
+    };
+  })();
+};
+
+replaceAll = function(str, find, replace) {
+  return str.replace(new RegExp(find, "g"), replace);
+};
+// Vanilla JavaScript RegEx (Trim Leading and Trailing)
+trimWhitespace = function(str) {
+  return str.replace(/^[\s\n]+|[\s\n]+$/g, "");
+};
+
+stripHtml = function(html) {
+  var tmp = document.createElement("DIV");
+  tmp.innerHTML = html;
+  return tmp.textContent || tmp.innerText || "";
+};
+
+generate_vari_value = function(vari, rand) {
+  var randy = rand();
+  return Math.floor(randy * (vari.max - vari.min) + vari.min);
+};
+
+generate_variables_eval = function(evaluated_variables) {
+  var evals = Object.keys(evaluated_variables).reduce(function(
+    eval_arr,
+    var_name
+  ) {
+    eval_arr.push(
+      "var " + var_name + " = " + evaluated_variables[var_name].value + ";"
+    );
+    return eval_arr;
+  },
+  []);
+  return evals.join("\n");
+};
+reduce_fraction = function(numerator, denominator) {
+  var gcd = function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+  };
+  gcd = gcd(numerator, denominator);
+  return [numerator / gcd, denominator / gcd];
+};
+
+evaluate_answer = function(evaluated_variables, answers) {
+  var varis_eval = generate_variables_eval(evaluated_variables);
+  var pre_eval = "";
+  if (Object.prototype.hasOwnProperty.call(answers, "pre_eval")) {
+    pre_eval = answers.pre_eval;
+  }
+  var evaluated_answers = Object.keys(answers).reduce(function(
+    evaluated_answers,
+    answer_name
+  ) {
+    if (answer_name != "pre_eval") {
+      var answer = answers[answer_name];
+      var evaluated = eval(varis_eval + ";" + pre_eval + ";" + answer.eval);
+      evaluated_answers[answer_name] = { value: evaluated };
+    }
+    return evaluated_answers;
+  },
+  {});
+  return evaluated_answers;
+};
+
+evaluate_rule = function(varis_eval, rule) {
+  eval(varis_eval);
+  return eval(rule);
+};
+
+evaluate_rules = function(rules, varis) {
+  var varis_eval = generate_variables_eval(varis);
+
+  for (var rule_index in rules) {
+    var rule = rules[rule_index].slice(0);
+    if (!evaluate_rule(varis_eval, rule)) {
+      console.log("failed rule", rule, varis);
+      return false;
+    }
+  }
+  return true;
+};
+
+generate_variable_values = function(variable_definitions, rules, rand) {
+  var attempt = 0;
+  var rules_pass = false;
+  while (attempt < 5 && rules_pass == false) {
+    attempt += 1;
+    var evaluated_variables = Object.keys(variable_definitions).reduce(function(
+      evaluated_variables,
+      var_name
+    ) {
+      variable_definition = variable_definitions[var_name];
+      evaluated_variables[var_name] = {
+        value: generate_vari_value(variable_definition, rand)
+      };
+      return evaluated_variables;
+    },
+    {});
+
+    rules_pass = evaluate_rules(rules, evaluated_variables);
+  }
+  return evaluated_variables;
+};
+
+fill_template = function(template, values) {
+  console.log("fill_template", template, values);
+  var templated = template.slice(0);
+  templated = Object.keys(values).reduce(function(templated, val_name) {
+    var value = values[val_name];
+    templated = replaceAll(templated, "%" + val_name + "%", value.value);
+    return templated;
+  }, templated);
+  return templated;
+};
+
+calculate_card = function(variables, rules, answers, template, rand) {
+  var varis = generate_variable_values(variables, rules, rand);
+  var answers = evaluate_answer(varis, answers);
+  console.log("answers", answers);
+  templated = fill_template(template, varis);
+  templated = fill_template(templated, answers);
+  return templated;
+};
+
+format_card_input = function(
+  variables_inp,
+  rules_inp,
+  answer_inp,
+  template_inp
+) {
+  var seedy = getSeed(template_inp.hashCode());
+  var rand = Alea(seedy);
+  variables = JSON.parse(variables_inp);
+  rules = JSON.parse(rules_inp);
+  answers = JSON.parse(answer_inp);
+  templated = calculate_card(variables, rules, answers, template_inp, rand);
+  document.getElementById("templated").innerHTML = templated;
+};
+
+process_card = function() {
+  var answer_inp = stripHtml(function() {
+    /*{{Answer}}*/
+  })
+    .toString()
+    .replace(/^[^\/]+\/\*!?/, "")
+    .replace(/\*\/[^\/]+$/, "");
+  var variables_inp = stripHtml(function() {
+    /*{{Variables}}*/
+  })
+    .toString()
+    .replace(/^[^\/]+\/\*!?/, "")
+    .replace(/\*\/[^\/]+$/, "");
+  var rules_inp = stripHtml(function() {
+    /*{{Rules}}*/
+  })
+    .toString()
+    .replace(/^[^\/]+\/\*!?/, "")
+    .replace(/\*\/[^\/]+$/, "");
+  var template_inp = function() {
+    /*{{Front}}*/
+  }
+    .toString()
+    .replace(/^[^\/]+\/\*!?/, "")
+    .replace(/\*\/[^\/]+$/, "");
+  format_card_input(variables_inp, rules_inp, answer_inp, template_inp);
+};
+(module.exports = generate_variable_values),
+  calculate_card,
+  reduce_fraction,
+  evaluate_answer,
+  generate_vari_value,
+  generate_variable_values;
---
+++ front.test.js
@@ -0,0 +1,175 @@
+require("./front");
+
+const RealDate = Date;
+
+describe("testcase", () => {
+  function mockDate(isoDate) {
+    global.Date = class extends RealDate {
+      constructor() {
+        return new RealDate(isoDate);
+      }
+    };
+  }
+  var variables = {
+    x1: { min: -10, max: 10 },
+    x2: { min: -10, max: 10 },
+    y1: { min: -10, max: 10 },
+    y2: { min: -10, max: 10 }
+  };
+  var answers = {
+    numerator: { eval: "y2-y1" },
+    denominator: { eval: "x2-x1" }
+  };
+  var front =
+    "Find the slope of the line containing the points (%x1%, %y1%) and (%x2%, %y2%).";
+
+  afterEach(() => {
+    global.Date = RealDate;
+  });
+
+  test("generate_vari_value -10 10", () => {
+    var seedy = getSeed("(y2-y1)/(x2-x1)".hashCode());
+    var rand = Alea(seedy);
+    var v = generate_vari_value(
+      {
+        name: "x1",
+        min: -10,
+        max: 10
+      },
+
+      rand
+    );
+    expect(v).toBeLessThanOrEqual(10);
+    expect(v).toBeGreaterThanOrEqual(-10);
+  });
+
+  test("generate_variable_values with extra prototype properties", () => {
+    var rules = [];
+    var rand = Alea(-1011170816);
+
+    expect(generate_variable_values(variables, rules, rand)).toEqual({
+      x1: {
+        value: 0
+      },
+      x2: {
+        value: 0
+      },
+      y1: {
+        value: -10
+      },
+      y2: {
+        value: 2
+      }
+    });
+  });
+
+  test("generate_variable_values on date when (x2-x1)==0 without foribidding rule", () => {
+    mockDate("2018-07-12T22:34:56z");
+    var rules = [];
+    var rand = Alea(-1011170816);
+
+    expect(generate_variable_values(variables, rules, rand)).toEqual({
+      x1: {
+        value: 0
+      },
+      x2: {
+        value: 0
+      },
+      y1: {
+        value: -10
+      },
+      y2: {
+        value: 2
+      }
+    });
+  });
+  test("generate_variable_values on date when (x2-x1)==0 with foribidding rule", () => {
+    mockDate("2018-07-12T22:34:56z");
+
+    var rules = ["(x2-x1) != 0"];
+    var rand = Alea(-1011170816);
+
+    expect(generate_variable_values(variables, rules, rand)).toEqual({
+      x1: {
+        value: 7
+      },
+      x2: {
+        value: -6
+      },
+      y1: {
+        value: -4
+      },
+      y2: {
+        value: 6
+      }
+    });
+    // but tries again when rules forbid x2-x1 to be 0
+  });
+
+  test("evaluate_rule x2-x1!=0 is true for 3 and 2", () => {
+    expect(evaluate_rule("var x1= 3; var x2 = 2;", "(x2-x1) != 0")).toEqual(
+      true
+    );
+  });
+  test("evaluate_rule (x2-x1)!=0 is false for 3 and 3", () => {
+    expect(evaluate_rule("var x1= 3; var x2 = 3;", "(x2-x1) != 0")).toEqual(
+      false
+    );
+  });
+  test("reduce_fraction 3/9", () => {
+    expect(reduce_fraction(3, 9)).toEqual([1, 3]);
+  });
+
+  test("reduce_fraction 1024/16384", () => {
+    expect(reduce_fraction(1024, 16384)).toEqual([1, 16]);
+  });
+
+  test("reduce_fraction 9/3", () => {
+    expect(reduce_fraction(9, 3)).toEqual([3, 1]);
+  });
+
+  test("reduce_fraction 0/9", () => {
+    expect(reduce_fraction(0, 9)).toEqual([0, 1]);
+  });
+
+  test("evaluate_answer ", () => {
+    var evaluated_variables = {
+      x1: {
+        value: 16
+      },
+      x2: {
+        value: 0
+      },
+      y1: {
+        value: 12
+      },
+      y2: {
+        value: 0
+      }
+    };
+    var simplified_answers = {
+      pre_eval:
+        "numerator = y2-y1; \
+      denominator = x2-x1; \
+      reduced = reduce_fraction(numerator, denominator);",
+      numerator: { eval: "reduced[0]" },
+      denominator: { eval: "reduced[1]" }
+    };
+
+    expect(evaluate_answer(evaluated_variables, simplified_answers)).toEqual({
+      denominator: { value: 4 },
+      numerator: { value: 3 }
+    });
+  });
+  test("calculate_card y2-y1/x2-x1 when (x2-x1)==0 and (x2-x1)!=0 rule", () => {
+    var rand = Alea(-1011170816);
+    var rules = ["(x2-x1) != 0"];
+    var front =
+      "Find the slope of the line containing the points (%x1%, %y1%) and (%x2%, %y2%). m = %numerator%/%denominator%";
+    var expectedFront =
+      "Find the slope of the line containing the points (7, -4) and (-6, 6). m = 10/-13";
+    expect(calculate_card(variables, rules, answers, front, rand)).toEqual(
+      expectedFront
+    );
+  });
+});
