81288f54f2577c818c443f87931862cad1a4b221
uncreative <ayelet.goldin@gmail.com>
2018-08-16 15:03:39 -0700

    move tests around

--- front.test.js
+++ front.test.js
@@ -2,14 +2,46 @@ require("./front");
 
 const RealDate = Date;
 
-describe("test generating problems with ranged variables", () => {
-  function mockDate(isoDate) {
-    global.Date = class extends RealDate {
-      constructor() {
-        return new RealDate(isoDate);
-      }
-    };
-  }
+test("evaluate_rule x2-x1!=0 is true for 3 and 2", () => {
+  expect(evaluate_rule("var x1= 3; var x2 = 2;", "(x2-x1) != 0")).toEqual(true);
+});
+test("evaluate_rule (x2-x1)!=0 is false for 3 and 3", () => {
+  expect(evaluate_rule("var x1= 3; var x2 = 3;", "(x2-x1) != 0")).toEqual(
+    false
+  );
+});
+test("reduce_fraction 3/9", () => {
+  expect(reduce_fraction(3, 9)).toEqual([1, 3]);
+});
+
+test("reduce_fraction 1024/16384", () => {
+  expect(reduce_fraction(1024, 16384)).toEqual([1, 16]);
+});
+
+test("reduce_fraction 9/3", () => {
+  expect(reduce_fraction(9, 3)).toEqual([3, 1]);
+});
+
+test("reduce_fraction 0/9", () => {
+  expect(reduce_fraction(0, 9)).toEqual([0, 1]);
+});
+
+test("generate_vari_value between min and max", () => {
+  var seedy = getSeed("(y2-y1)/(x2-x1)".hashCode());
+  var rand = Alea(seedy);
+  var v = generate_vari_value(
+    {
+      min: -10,
+      max: 10
+    },
+
+    rand
+  );
+  expect(v).toBeLessThanOrEqual(10);
+  expect(v).toBeGreaterThanOrEqual(-10);
+});
+
+describe("test generating variables with ranged variables", () => {
   var variables = {
     x1: { min: -10, max: 10 },
     x2: { min: -10, max: 10 },
@@ -23,25 +55,6 @@ describe("test generating problems with ranged variables", () => {
   var front =
     "Find the slope of the line containing the points (%x1%, %y1%) and (%x2%, %y2%).";
   var bad_values_seed = -1011170816;
-
-  afterEach(() => {
-    global.Date = RealDate;
-  });
-
-  test("generate_vari_value between min and max", () => {
-    var seedy = getSeed("(y2-y1)/(x2-x1)".hashCode());
-    var rand = Alea(seedy);
-    var v = generate_vari_value(
-      {
-        min: -10,
-        max: 10
-      },
-
-      rand
-    );
-    expect(v).toBeLessThanOrEqual(10);
-    expect(v).toBeGreaterThanOrEqual(-10);
-  });
 
   test("generate_variable_values x2-x1 == 0 when using bad_values_seed", () => {
     var rules = [];
@@ -82,31 +95,28 @@ describe("test generating problems with ranged variables", () => {
     });
     // but tries again when rules forbid x2-x1 to be 0
   });
+});
 
-  test("evaluate_rule x2-x1!=0 is true for 3 and 2", () => {
-    expect(evaluate_rule("var x1= 3; var x2 = 2;", "(x2-x1) != 0")).toEqual(
-      true
-    );
-  });
-  test("evaluate_rule (x2-x1)!=0 is false for 3 and 3", () => {
-    expect(evaluate_rule("var x1= 3; var x2 = 3;", "(x2-x1) != 0")).toEqual(
-      false
-    );
-  });
-  test("reduce_fraction 3/9", () => {
-    expect(reduce_fraction(3, 9)).toEqual([1, 3]);
-  });
+describe("test generating problems with simplified fraction answer", () => {
+  function mockDate(isoDate) {
+    global.Date = class extends RealDate {
+      constructor() {
+        return new RealDate(isoDate);
+      }
+    };
+  }
 
-  test("reduce_fraction 1024/16384", () => {
-    expect(reduce_fraction(1024, 16384)).toEqual([1, 16]);
-  });
-
-  test("reduce_fraction 9/3", () => {
-    expect(reduce_fraction(9, 3)).toEqual([3, 1]);
-  });
-
-  test("reduce_fraction 0/9", () => {
-    expect(reduce_fraction(0, 9)).toEqual([0, 1]);
+  var simplified_answers = {
+    pre_eval:
+      "numerator = y2-y1; \
+        denominator = x2-x1; \
+        reduced = reduce_fraction(numerator, denominator);",
+    numerator: { eval: "reduced[0]" },
+    denominator: { eval: "reduced[1]" }
+  };
+  var rules = ["(x2-x1) != 0"];
+  afterEach(() => {
+    global.Date = RealDate;
   });
 
   test("evaluate_answer is 4/3 for (12-0)/(16-0) with pre_eval that simpilifies fractions ", () => {
@@ -124,14 +134,6 @@ describe("test generating problems with ranged variables", () => {
         value: 0
       }
     };
-    var simplified_answers = {
-      pre_eval:
-        "numerator = y2-y1; \
-      denominator = x2-x1; \
-      reduced = reduce_fraction(numerator, denominator);",
-      numerator: { eval: "reduced[0]" },
-      denominator: { eval: "reduced[1]" }
-    };
 
     expect(evaluate_answer(evaluated_variables, simplified_answers)).toEqual({
       denominator: { value: 4 },
@@ -140,15 +142,13 @@ describe("test generating problems with ranged variables", () => {
   });
   test("calculate_card generates templates (7,-4) and (-6,6) as well as m=10/-13", () => {
     mockDate("2018-07-17T22:34:56z");
-    var simplified_answers = {
-      pre_eval:
-        "numerator = y2-y1; \
-      denominator = x2-x1; \
-      reduced = reduce_fraction(numerator, denominator);",
-      numerator: { eval: "reduced[0]" },
-      denominator: { eval: "reduced[1]" }
+    var variables = {
+      x1: { min: -10, max: 10 },
+      x2: { min: -10, max: 10 },
+      y1: { min: -10, max: 10 },
+      y2: { min: -10, max: 10 }
     };
-    var rules = ["(x2-x1) != 0"];
+
     var front =
       "Find the slope of the line containing the points (%_x1_%, %_y1_%) and (%_x2_%, %_y2_%).";
     var rand = Alea(getSeed(front.hashCode()));
